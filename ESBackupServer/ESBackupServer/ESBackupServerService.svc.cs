d5f41b9e4778c85fe9e7084be45334e3c7fe3064
MrkacekVladimir <vlad.mrkacek@gmail.com>
2017-03-25 20:52:54 +0100

    Rename
    Přejmenování Servicu

---
+++ ESBackupServer/ESBackupServer/ESBackupServerService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ESBackupServer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ESBackupServerService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ESBackupServerService.svc or ESBackupServerService.svc.cs at the Solution Explorer and start debugging.
+    public class ESBackupServerService : IESBackupServerService
+    {
+        public void Test()
+        {            
+        }
+    }
+}
