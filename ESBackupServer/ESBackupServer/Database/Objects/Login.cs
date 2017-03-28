369bf052426f2970c1516813bda586c06eb389ec
MrkacekVladimir <vlad.mrkacek@gmail.com>
2017-03-28 20:04:25 +0200

    DataContract - Login
    Přidání DataContractu a DataMemberu do Loginu

--- ESBackupServer/ESBackupServer/Database/Objects/Login.cs
+++ ESBackupServer/ESBackupServer/Database/Objects/Login.cs
@@ -1,29 +1,30 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
 
 namespace ESBackupServer.Database.Objects
 {
-    [Table("esbk_tbLogins")]
+    [Table("esbk_tbLogins"), DataContract]
     public class Login
     {
         #region EntityFramework
-        [Key, Column("ID")]
+        [Key, Column("ID"), DataMember]
         public Guid ID { get; set; }
 
-        [Column("IDesbk_tbClients")]
+        [Column("IDesbk_tbClients"), DataMember]
         public int IDClient { get; set; }
 
-        [Column("LG_TIME_UTC")]
+        [Column("LG_TIME_UTC"), DataMember]
         public DateTime UTCTime { get; set; }
 
-        [Column("LG_CLIENT_IP")]
+        [Column("LG_CLIENT_IP"), DataMember]
         public byte[] IP { get; set; } //IPv4 - 32; IPv6 - 128
 
-        [Column("LG_ACTIVE")]
+        [Column("LG_ACTIVE"), DataMember]
         public bool Active { get; set; }
 
-        [ForeignKey("IDClient")]
+        [ForeignKey("IDClient"), DataMember]
         public virtual Client Client { get; set; }
         #endregion
 
