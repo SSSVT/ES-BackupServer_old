21eb532e6919291b162d15cf916dca4e26abd363
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2017-04-04 21:32:35 +0200

    EmailSender added

---
+++ ESBackupServer/ESBackupServer/App/Components/Net/Mail/MailSender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ESBackupServer.App.Components.Net.Mail
+{
+    internal class MailSender
+    {
+        public void Send()
+        {
+            //TODO: Implement sending emails
+            throw new NotImplementedException();
+        }
+    }
+}
