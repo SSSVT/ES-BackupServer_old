c07efc1d05b1e60849196de416c7610d27bdba77
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2017-04-06 11:40:43 +0200

    DatabaseContext edited, EmailFactory added

--- ESBackupServer/ESBackupServer/App/Components/Net/Mail/MailSender.cs
+++ ESBackupServer/ESBackupServer/App/Components/Net/Mail/MailSender.cs
@@ -6,18 +6,25 @@ namespace ESBackupServer.App.Components.Net.Mail
 {
     internal class MailSender
     {
-        public void Send(string server, string recipient, string subject, string message)
+        public void Send(string server, string recipient, string subject, string message, bool EnableSSL = false)
         {
-            this.Send(new SmtpClient(server), new MailMessage(Properties.Settings.Default.MailSender, recipient, subject, message));
+            this.Send(
+                new SmtpClient(server)
+                {
+                    EnableSsl = EnableSSL
+                },
+                new MailMessage(Properties.Settings.Default.MailSender, recipient, subject, message));
         }
-        public void Send(string server, string username, string password, string recipient, string subject, string message)
+        public void Send(string server, string username, string password, string recipient, string subject, string message, bool EnableSSL = false)
         {
-            SmtpClient client = new SmtpClient(server)
-            {
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential(username, password)
-            };
-            this.Send(new SmtpClient(server), new MailMessage(Properties.Settings.Default.MailSender, recipient, subject, message));
+            this.Send(
+                new SmtpClient(server)
+                {
+                    EnableSsl = EnableSSL,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(username, password)
+                },
+                new MailMessage(Properties.Settings.Default.MailSender, recipient, subject, message));
         }
 
         private void Send(SmtpClient client, MailMessage message)
---
+++ ESBackupServer/ESBackupServer/App/Objects/Factories/Net/Mail/MailFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ESBackupServer.App.Objects.Factories.Net.Mail
+{
+    public class MailFactory
+    {
+        public string CreateBody()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreateSubject()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
